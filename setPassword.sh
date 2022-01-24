a1110a935bde78d1a8fefc9bbba6aafaf2f69c47
Jozborne <98144813+Jozborne@users.noreply.github.com>
2022-01-24 17:20:03 +1100

    Update setPassword.sh
    Capitalisation corrected. Further update formatting.

--- setPassword.sh
+++ setPassword.sh
@@ -1,16 +1,31 @@
 #! /bin/bash
+
+#Set Password.sh 
+
+#Version 1.0
+
+#Lets the User enter the name of the folder
+
 read -p "Name of folder you wish to create : " folderName
-# Lets the User enter the name of the folder
+
+#Displays "Enter Password" to the user
+
 echo "Enter Password"
-#Displays "Enter Password to the user"
-read -sp 'password : ' pass_var
+
 #Hides the password input 
 
+read -sp 'password : ' pass_var
+
+#Creates the folder name in the week2 folder tree
+
 mkdir ~/john/scripts/portfolio/week2/"$folderName"
-#creates the folder name in the week2 folder tree
-touch ~/john/scripts/portfolio/week2/"$folderName"/secret.txt
+
 #Creates the secret.txt document
+
+touch ~/john/scripts/portfolio/week2/"$folderName"/secret.txt
+
 echo "$pass_var" > ~/john/scripts/portfolio/week2/"$folderName"/secret.txt
+
 #Displays the $pass_var as enter previously in the created file tree and secret.txt document
 
 exit 0
