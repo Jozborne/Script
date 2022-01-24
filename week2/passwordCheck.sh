4d20496f8baaf57572336f204837302eb58b8f52
Jozborne <98144813+Jozborne@users.noreply.github.com>
2022-01-24 21:50:59 +1100

    Add files via upload
    Files uploaded for unformity

---
+++ week2/passwordCheck.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+#Created by John Osborne
+#Creation 24 January 2022
+#Purpose of script is to check hashed password and grant or deny access
+
+
+#Displays "Enter Password to the user"
+echo "Enter Secret Password"
+
+#Hides the password input 
+read -sp 'password : ' pass_var
+
+#Variable created to Sha256 assigned var_hash with the user inputted pass_var being hashed
+
+var_hash=$(echo $pass_var | sha256sum)
+
+#Created a myvariable command which displays the hash that was created with the cat command
+
+myvariable=$(cat "/home/kali/john/scripts/portfolio/week2/secret.txt")
+
+#Displays the  var_hash which is changed by what the user inputs.
+
+echo "1: $var_hash"
+
+#Is the password that was created previously by the user in 2.2.
+
+echo "2: $myvariable"
+
+#If statements with $var_hash user entry having to equal $myvariable
+
+if [ "$var_hash" = "$myvariable" ]; then
+
+#If the variables match access is granted 
+
+    echo "Access Granted" 
+#exit command given if successful
+
+exit 0
+#If the $var_hash and $myvariable are not equal Access is Denied
+else
+    echo "Access Denied"
+# Ends If statement 
+fi
+#Exits script 
+exit 1
+
+
---
+++ week2/setPassword.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+#Created by John Osborne
+#Creation 23 January 2022
+#Modification date 24 January 2022
+#Purpose of script is to hash a password in a folder to a .txt document.
+
+#Lets the User enter the name of the folder
+
+read -p "Name of folder you wish to create : " folderName
+
+#Displays "Enter Password" to the user
+
+echo "Enter Password"
+
+#Hides the password input
+
+read -sp 'password : ' pass_var
+
+#Creates the folder name in the week2 folder tree
+
+mkdir ~/john/scripts/portfolio/week2/"$folderName"
+
+#Creates the secret.txt document (Rani)
+
+touch ~/john/scripts/portfolio/week2/"$folderName"/secret.txt
+
+#Displays the $pass_var as entered previously however this is now hashed in the created file to secret.txt document
+
+echo $pass_var | sha256sum > ~/john/scripts/portfolio/week2/"$folderName"/secret.txt
+
+exit 0
+
+#Rani, B. (2021, 19 February 2021). touch command in Linux with Examples. Retrieved 23 January from https://www.geeksforgeeks.org/touch-command-in-linux-with-examples
