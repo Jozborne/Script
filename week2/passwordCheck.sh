94d08b2cb010bca7139a7bee9a5d11e719360f84
Jozborne <98144813+Jozborne@users.noreply.github.com>
2022-01-24 22:54:39 +1100

    Add files via upload
    Removed unnecessary echo that produced a hash on the user's screen. Not require once testing and fault finding had been completed,

--- week2/passwordCheck.sh
+++ week2/passwordCheck.sh
@@ -8,7 +8,7 @@
 echo "Enter Secret Password"
 
 #Hides the password input 
-read -sp 'password : ' pass_var
+read -sp 'Password : ' pass_var
 
 #Variable created to Sha256 assigned var_hash with the user inputted pass_var being hashed
 
@@ -18,15 +18,7 @@ var_hash=$(echo $pass_var | sha256sum)
 
 myvariable=$(cat "/home/kali/john/scripts/portfolio/week2/John/secret.txt")
 
-#Displays the  var_hash which is changed by what the user inputs.
-
-echo "1: $var_hash"
-
-#Is the password that was created previously by the user in 2.2.
-
-echo "2: $myvariable"
-
-#If statements with $var_hash user entry having to equal $myvariable
+#Removed echo's that were not required where the hash was presented unencrypted to the users screen.
 
 if [ "$var_hash" = "$myvariable" ]; then
 
@@ -35,13 +27,13 @@ if [ "$var_hash" = "$myvariable" ]; then
     echo "Access Granted" 
 #exit command given if successful
 
-exit 0
+    exit 0
 #If the $var_hash and $myvariable are not equal Access is Denied
 else
     echo "Access Denied"
 # Ends If statement 
 fi
 #Exits script 
-exit 1
+    exit 1
 
 
