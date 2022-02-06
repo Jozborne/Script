e2892a3ee847b9e832827b41d994a105c4131e98
Jozborne <98144813+Jozborne@users.noreply.github.com>
2022-02-06 19:00:18 +1100

    Add files via upload
    Updated format to improve readability.

--- week3/webdownloader.sh
+++ week3/webdownloader.sh
@@ -1,29 +1,35 @@
-#!/bin/bash
+#! /bin/bash
+#Created by John Osborne
+#Creation 30 January 2022
+#Modified 6 February 2022s
+#Created to allow user to download webpages.
 
 
-#start on infinite loop
+#Creates loop
 
-
-
-
-
-
-
-while :;
+while true
 do
 
-read -p "Input the download URL or exit to quit:" url
+read -p "Enter URL :" webaddress
+#Allows command to be exited on enter of exit
 
-if [ "$url" == "exit" ];then break;
-fi
-read -p "Input the directory where you wish to save: " directory
+    if [ "$webaddress" == "exit" ];then 
+        echo "Thank you for using this script"
+        exit;
+    fi
+    
+#Allows user to select where they wish to store the file
+read -p "Where do you want to save the web address?: " 
 
-wget $url
+#Allow wget to run and download 
+
+wget "$webaddress"
 
 
+#Allows command to be exited on enter of exit
 
 
-
+#Geeks, G. f. (2021, 08/09/2021). Wget command in Linux/Unix. Retrieved 30 January from https://www.geeksforgeeks.org/wget-command-in-linux-unix/#:~:text=Wget%20is%20the%20non%2Dinteractive,without%20hindering%20the%20current%20process.
 
 
 
