5d352e3b09d64a8ccdcb83b8d9e0739a791fb204
Jozborne <98144813+Jozborne@users.noreply.github.com>
2022-02-27 11:28:22 +1100

    Add files via upload
    Purpose updated date modified updated Final check before submission.

--- week1/first.sh
+++ week1/first.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 #Created by John Osborne
 #Created on 21 January 2022 
-#Updated on the 6 February 2022
+#Updated on the 26 February 2022
+#Purpose echo Hi there! then exit 
 echo "Hi there!" 
       
 exit 22
--- week1/second.sh
+++ week1/second.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Created by John Osborne
 #Created on the 21 January 2022
-#Updated on the 6 February 2022
-
+#Updated on the 26 February 2022
+#Purpose introduction to variables where name after running the script = $1
 echo "Hi there!"
 
 echo "It's good to see you $1!"
